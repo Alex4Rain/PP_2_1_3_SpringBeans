7ff2ecf43a119b0aa24ceeb33741572d114be194
Alex4Rain <alex4rain@gmail.com>
2022-07-04 23:53:16 +0300

    complete

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,8 @@
 package app.model;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
