263b0072c98c18a2d83ff8781adf88271e39d444
Alex4Rain <alex4rain@gmail.com>
2022-07-04 23:43:45 +0300

    complete

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,13 +7,20 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    private Dog animal;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+
+    }
+    public Timer getTimer() {
+        return timer;
+
     }
 }
